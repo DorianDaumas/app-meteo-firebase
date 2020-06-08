c6c2ef83686461ae65ac0aa4e6876e7e1e6293ce
DorianDaumas <dorian.daumas@epitech.eu>
2020-06-08 19:00:48 +0200

    deploy vue

---
+++ precache-manifest.806f266594ba0aae2886f1f0de2131db.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d8a0e0431105942184",
+    "url": "/css/app.174899ad.css"
+  },
+  {
+    "revision": "0d09f41c522bf081acbf",
+    "url": "/css/chunk-vendors.1392eb48.css"
+  },
+  {
+    "revision": "c20b50ba516619ef0131d9665d42241f",
+    "url": "/img/arrow.c20b50ba.svg"
+  },
+  {
+    "revision": "bde7f16e27ce19725280a51312d62e44",
+    "url": "/img/deconnexion.bde7f16e.svg"
+  },
+  {
+    "revision": "3979298b342aff9f46a86a0225756066",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d8a0e0431105942184",
+    "url": "/js/app.a02a41e4.js"
+  },
+  {
+    "revision": "0d09f41c522bf081acbf",
+    "url": "/js/chunk-vendors.762c658c.js"
+  },
+  {
+    "revision": "5f3976fe2eb0744138ce91bdc564c1ca",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
